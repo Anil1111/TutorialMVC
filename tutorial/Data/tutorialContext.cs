72b4d48103462f898666577afd19c496c809889d
MiguelPires1904 <miguelpiires.15@gmail.com>
2018-10-14 23:47:10 +0100

    TutorialMVC

---
+++ tutorial/Data/tutorialContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace tutorial.Models
+{
+    public class tutorialContext : DbContext
+    {
+        public tutorialContext (DbContextOptions<tutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
